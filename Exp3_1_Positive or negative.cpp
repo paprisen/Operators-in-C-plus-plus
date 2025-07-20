8a638005cf3651e827f7341da5e35bdc65f16738
Papri <papri.sen2020@gmail.com>
2025-07-20 12:00:30 +0530

    Create Exp3_1_Positive or negative.cpp

---
+++ Exp3_1_Positive or negative.cpp
@@ -0,0 +1,24 @@
+// PAPRI SEN
+// 24070123070
+// A3
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout << "Enter a number:";
+    cin >> a;
+    if(a > 0)
+    {
+        cout << "Entered Number is Positive"; 
+    }
+    else if(a < 0) 
+    {
+        cout << "Entered Number is Negative"; 
+    }
+    else 
+    {
+        cout << "Entered Number is 0"; 
+    }
+    return 0;
+}
